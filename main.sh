00c57adeeffade595da3b4764c0af74bc7ba3a1e
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-29 01:56:30 +0300

    port to pika os 4

--- main.sh
+++ main.sh
@@ -1,18 +1,26 @@
-DEBIAN_FRONTEND=noninteractive
+#! /bin/bash
+
+set -e
+
+VERSION="1.25.1.0.1"
+
+source ./pika-build-config.sh
+
+echo "$PIKA_BUILD_ARCH" > pika-build-arch
 
 # Clone Upstream
 mkdir -p ./pikman
-cp -rvf ./* ./pikman || echo
+cp -rvf ./* ./pikman || true
 cd ./pikman
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-dpkg-buildpackage
+LOGNAME=root dh_make --createorig -y -l -p pikman_"$VERSION" || echo "dh-make: Ignoring Last Error"
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
 mkdir -p ./output
 mv ./*.deb ./output/
-
